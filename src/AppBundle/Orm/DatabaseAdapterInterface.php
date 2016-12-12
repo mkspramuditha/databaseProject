2a35336ee69e4b504f37d7090b3b0675d6bdf371
mkspramuditha <mkspramuditha@gmail.com>
2016-12-12 22:02:27 +0530

    databaseAdapterInterface created

---
+++ src/AppBundle/Orm/DatabaseAdapterInterface.php
@@ -0,0 +1,27 @@
+<?php
+namespace AppBundle\Orm;
+
+Interface DatabaseAdapter{
+    function connect();
+
+    function disconnect();
+
+    function query($query);
+
+    function fetch();
+
+    function select($table, $conditions = "", $fields = '*', $order = "", $limit = null, $offset = null);
+
+    function insert($table, array $data);
+
+    function update($table, array $data, $conditions);
+
+    function delete($table, $conditions);
+
+    function getInsertId();
+
+    function countRows();
+
+    function getAffectedRows();
+}
+
