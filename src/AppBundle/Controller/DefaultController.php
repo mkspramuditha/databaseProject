5f6139b44f044c4ce7de60d2db769c66b3864f59
mkspramuditha <mkspramuditha@gmail.com>
2016-12-14 22:10:03 +0530

    resources added

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,12 +10,46 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/login", name="adminLogin")
      */
-    public function indexAction(Request $request)
+    public function loginAction(Request $request)
     {
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+
+        $auth_checker = $this->get('security.authorization_checker');
+
+
+        $token = $this->get('security.token_storage')->getToken();
+
+        $user = $token->getUser();
+
+
+
+        $isRoleAdmin = $auth_checker->isGranted('ROLE_ADMIN');
+        if($isRoleAdmin)
+        {
+            return $this->redirect(
+                $this->generateUrl("adminDashboard")
+            );
+        }
+
+
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        $error =$authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUserName();
+        return $this->render('default/adminLogin.html.twig',array(
+            'last_username' =>$lastUsername,
+            'error'=>$error
         ));
+
+    }
+
+    /**
+     * @Route("/dashboard", name="adminDashboard")
+     */
+    public function dashboardAction(Request $request)
+    {
+        var_dump('dashboard');
+        exit;
     }
 }
