6e16b0b521032a79e33c6d04e64400b24256b78a
mkspramuditha <mkspramuditha@gmail.com>
2016-12-14 20:34:24 +0530

    user entity created

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,16 +3,19 @@
 namespace AppBundle\Entity;
 
 use AppBundle\Orm\AbstractEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-class User extends AbstractEntity
+class User extends AbstractEntity implements UserInterface, \Serializable
 {
     private $_tableName = 'users';
     private $_repositoryName = 'UsersRepository';
     private $_fieldNames = ['Username','Password','Email'];
 
+    private $id;
     private $username;
     private $password;
     private $email;
+    private $roles;
 
 
     public function __construct()
@@ -67,6 +70,38 @@ class User extends AbstractEntity
     /**
      * @return mixed
      */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * @param mixed $roles
+     */
+    public function setRoles($roles)
+    {
+        $this->roles = $roles;
+    }
+
+    /**
+     * @return mixed
+     */
     public function getEmail()
     {
         return $this->email;
@@ -95,5 +130,41 @@ class User extends AbstractEntity
     {
         $this->_fieldNames = $fieldNames;
     }
+
+    public function eraseCredentials()
+    {
+    }
+
+    /** @see \Serializable::serialize() */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
+            // see section on salt below
+            // $this->salt,
+        ));
+    }
+
+    /** @see \Serializable::unserialize() */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+            // see section on salt below
+            // $this->salt
+            ) = unserialize($serialized);
+    }
+
+    public function getSalt()
+    {
+        // you *may* need a real salt depending on your encoder
+        // see section on salt below
+        return null;
+    }
+
 
 }
