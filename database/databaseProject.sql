6411a1e27f88a8805e28873b7868d4c572f891ca
pushpa66 <nipuna.14@cse.mrt.ac.lk>
2016-12-16 22:19:56 +0530

    database modified

--- database/databaseProject.sql
+++ database/databaseProject.sql
@@ -1,10 +1,10 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Dec 14, 2016 at 05:56 ප.ව.
--- Server version: 10.1.19-MariaDB
+-- Host: 127.0.0.1
+-- Generation Time: Dec 16, 2016 at 05:48 PM
+-- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -17,8 +17,65 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `databaseProject`
+-- Database: `databaseproject`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `diseasedata`
+--
+
+CREATE TABLE `diseasedata` (
+  `id` int(11) NOT NULL,
+  `userid` varchar(255) NOT NULL,
+  `diseasedataid` varchar(255) NOT NULL,
+  `symptoms` varchar(1023) NOT NULL,
+  `description` varchar(1023) NOT NULL,
+  `victimcount` int(11) NOT NULL,
+  `locationid` varchar(255) NOT NULL,
+  `entryid` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `entrydetails`
+--
+
+CREATE TABLE `entrydetails` (
+  `id` int(11) NOT NULL,
+  `entryid` varchar(255) NOT NULL,
+  `datetime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+CREATE TABLE `location` (
+  `id` int(11) NOT NULL,
+  `locationcode` varchar(255) NOT NULL,
+  `locationname` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userdetails`
+--
+
+CREATE TABLE `userdetails` (
+  `id` int(11) NOT NULL,
+  `userid` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `secondname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -45,15 +102,64 @@ INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
 --
 
 --
+-- Indexes for table `diseasedata`
+--
+ALTER TABLE `diseasedata`
+  ADD PRIMARY KEY (`diseasedataid`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `entrydetails`
+--
+ALTER TABLE `entrydetails`
+  ADD PRIMARY KEY (`entryid`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`locationcode`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `userdetails`
+--
+ALTER TABLE `userdetails`
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `userid` (`userid`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`username`),
+  ADD UNIQUE KEY `id` (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `diseasedata`
+--
+ALTER TABLE `diseasedata`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `entrydetails`
+--
+ALTER TABLE `entrydetails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `location`
+--
+ALTER TABLE `location`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `userdetails`
+--
+ALTER TABLE `userdetails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `users`
 --
