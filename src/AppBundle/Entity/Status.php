1c3ec31f74d3d39d716cbbf55bf5452bff9127d4
pushpa66 <nipuna.14@cse.mrt.ac.lk>
2016-12-18 13:17:50 +0530

    status adding 50% checking 100% OK

---
+++ src/AppBundle/Entity/Status.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use AppBundle\Orm\AbstractEntity;
+use Symfony\Component\Security\Core\User\EquatableInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class Status extends AbstractEntity
+{
+    private $_tableName = 'status';
+    private $_repositoryName = 'StatusRepository';
+    private $_fieldNames = ['Id', 'StatusId', 'StatusName'];
+    private $_columnNames = ['id', 'statusId', 'statusName'];
+
+    private $id;
+    private $statusId;
+    private $statusName;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatusId()
+    {
+        return $this->statusId;
+    }
+
+    /**
+     * @param mixed $statusId
+     */
+    public function setStatusId($statusId)
+    {
+        $this->statusId = $statusId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatusName()
+    {
+        return $this->statusName;
+    }
+
+    /**
+     * @param mixed $statusName
+     */
+    public function setStatusName($statusName)
+    {
+        $this->statusName = $statusName;
+    }
+    //=================================================================
+
+
+
+//=================================================================
+    public function __construct()
+    {
+        $this->setTableName($this->_tableName);
+        $this->setRepositoryName($this->_repositoryName);
+        $this->setFieldNames($this->_fieldNames);
+
+    }
+
+    public function callGetMethod($name)
+    {
+        $object = $this;
+        return function () use ($object, $name) {
+            $args = func_get_args();
+            return call_user_func_array(array($object, $name), $args);
+        };
+    }
+
+    /**
+     * @return string
+     */
+    public function getTableName()
+    {
+        return $this->_tableName;
+    }
+
+    /**
+     * @param string $tableName
+     */
+    public function setTableName($tableName)
+    {
+        $this->_tableName = $tableName;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getRepositoryName()
+    {
+        return $this->_repositoryName;
+    }
+
+    /**
+     * @param string $repositoryName
+     */
+    public function setRepositoryName($repositoryName)
+    {
+        $this->_repositoryName = $repositoryName;
+    }
+
+    /**
+     * @return array
+     */
+    public function getColumnNames()
+    {
+        return $this->_columnNames;
+    }
+
+    /**
+     * @param array $columnNames
+     */
+    public function setColumnNames($columnNames)
+    {
+        $this->_columnNames = $columnNames;
+    }
+
+
+    /**
+     * @return array
+     */
+    public function getFieldNames()
+    {
+        return $this->_fieldNames;
+    }
+
+    /**
+     * @param array $fieldNames
+     */
+    public function setFieldNames($fieldNames)
+    {
+        $this->_fieldNames = $fieldNames;
+    }
+
+
+}
